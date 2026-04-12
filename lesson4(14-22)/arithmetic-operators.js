0b8d48de0208b68fb355264ee03a9af9d33a530e
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-13 00:36:12 +0200

    add arith operators

--- lesson4(14-22)/arithmetic-operators.js
+++ lesson4(14-22)/arithmetic-operators.js
@@ -1,3 +1,29 @@
 console.log(10 + 20);
 console.log(10 + "Hamza");
-console.log(10 + 20);
+
+console.log(10 - 20);
+console.log(10 - "Hamza");
+
+console.log(10 * 20);
+console.log(10 * -20);
+
+console.log(2 ** 4);
+console.log(16 ** (1 / 2));
+console.log(10 ** -1);
+
+console.log(10 % 2);
+console.log(11 % 2);
+
+let num = 1;
+
+console.log(num++); // 1
+console.log(++num); // 3
+
+num = 1;
+
+console.log(num--); // 1
+console.log(--num); // -1
+
+console.log(typeof NaN);
+console.log(Infinity / Infinity);
+console.log(undefined - undefined)
