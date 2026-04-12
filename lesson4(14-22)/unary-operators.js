9eaa5731fe1e2866fe6d281b6f6dd2c0cff42e45
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-13 00:40:36 +0200

    add unary operators

---
+++ lesson4(14-22)/unary-operators.js
@@ -0,0 +1,26 @@
+console.log(+100);
+console.log(+"100");
+console.log(+"-100");
+console.log(+"100.5");
+console.log(+"Hamza");
+console.log(+0xff);
+console.log(+null);
+console.log(+false);
+console.log(+true);
+
+console.log("==========================");
+console.log("==========================");
+
+console.log(-100);
+console.log(-"100");
+console.log(-"-100");
+console.log(-"100.5");
+console.log(-"Hamza");
+console.log(-0xff);
+console.log(-null);
+console.log(-false);
+console.log(-true);
+
+// Convert string to number, deep later . . .
+console.log(+"100"); // First Way
+console.log(Number("100")); // Second Way
