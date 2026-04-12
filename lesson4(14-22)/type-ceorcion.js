c0ad8174acbb1f303da2c2a02d788132dbe00c68
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-13 00:51:22 +0200

    add type coercion

---
+++ lesson4(14-22)/type-ceorcion.js
@@ -0,0 +1,15 @@
+let a = "10";
+let b = 20;
+let c = true;
+let d = false;
+let e = "Hamza";
+
+console.log(a + b); // 1020
+console.log(+a + b); // 30
+console.log(-a + b); // 10
+console.log(+""); // 0
+console.log("" - 2); // -2
+console.log(a - b); // -10
+console.log(b - e); // NaN
+console.log(a + b + c); // 1020true
+console.log(+a + b + c); // 31
