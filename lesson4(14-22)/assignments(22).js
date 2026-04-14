4c4cc439723e97e01a5d5fec83fc123fcd0c2a79
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-14 13:43:33 +0200

    solve assignments(18-22)

---
+++ lesson4(14-22)/assignments(22).js
@@ -0,0 +1,54 @@
+// Assignment 1
+// Replace ? With Arithmetic Operators
+console.log(10 + 20 * 15 % 3 * 190 - 10 % 400); // 0    
+
+// Assignment 2
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num / (true / (true + true))); // 6
+
+// Soultion Three
+console.log(num ** (true + true) - num); // 6
+
+// Soultion Four
+console.log(num * num - num); // 6
+
+// Solution Five
+console.log(++num + --num - true); // 6
+
+// Solution Six
+console.log(++num + true + true); // 6
+
+// Assignment 3
+
+let num2 = "10";
+
+// Solution One
+console.log(num2 * (true + true)); // 20
+
+// Solution Two
+console.log(num2 / (true / (true + true))); // 20
+
+// Solution Three
+console.log(+num2 + +num2); // 20
+
+// Solution Four
+console.log(num2 - -num2); // 20
+
+// Assignment 4
+
+let points = 10;
+
+// Write Your Code Here
+points += true + true + true;
+
+console.log(points); // 13
+
+// Write Your Code Here
+points -= true + true + true + true + true
+
+console.log(points); // 8;
