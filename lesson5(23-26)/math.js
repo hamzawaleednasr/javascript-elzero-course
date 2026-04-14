1bd755a9b908e0a2c6d160afc29a485ddc4063c4
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-14 14:15:51 +0200

    add math

---
+++ lesson5(23-26)/math.js
@@ -0,0 +1,21 @@
+console.log(Math.round(100.6));
+console.log(Math.round("100.6"));
+console.log(Math.round(100.2));
+
+console.log(Math.floor(100.9));
+console.log(Math.floor(100.2));
+console.log(Math.floor("100.2"));
+console.log(Math.floor(-100.9));
+
+console.log(Math.ceil(100.1));
+console.log(Math.ceil(100.7));
+console.log(Math.ceil("100.7"));
+console.log(Math.ceil(-100.1));
+
+console.log(Math.trunc("100.8"));
+console.log(Math.trunc(100.1));
+
+console.log(Math.pow(4, 2));
+
+console.log(Math.random());
+console.log(Math.trunc(Math.random() * 100)); // random from 0 to 99
