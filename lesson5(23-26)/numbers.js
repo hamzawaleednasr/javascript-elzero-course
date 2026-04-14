cddee781ccaa1349b4ffe31d8abf1b29e04248e4
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-14 14:10:25 +0200

    add numbers methods

---
+++ lesson5(23-26)/numbers.js
@@ -0,0 +1,6 @@
+console.log(1000000);
+console.log(1_000_000);
+console.log(1000000.0);
+console.log(1e6);
+console.log(10 ** 6);
+console.log(10 * 10 * 10 * 10 * 10 * 10);
---
+++ lesson5(23-26)/numbers_methods.js
@@ -0,0 +1,22 @@
+console.log((100).toString());
+console.log(100..toString());
+console.log(100.12.toString());
+
+console.log(100.764387.toFixed(3));
+
+console.log(Number("100"));
+console.log(+"100");
+console.log(parseInt("100"));
+console.log(parseInt("100 Hamza"));
+console.log(parseInt("Hamza 100 Hamza"));
+console.log(parseInt("100.16 Hamza"));
+console.log(parseFloat("100.16 Hamza"));
+
+console.log(Number.isInteger("Hamza"));
+console.log(Number.isInteger(100.72));
+console.log(Number.isInteger(100));
+
+console.log(Number.isNaN("Hamza"));
+console.log(Number.isNaN("Hamza" / 100));
+console.log(Number.isNaN(NaN));
+
