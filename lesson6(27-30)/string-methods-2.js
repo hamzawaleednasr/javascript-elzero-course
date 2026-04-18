be77f9b4954e8a7f9c6d505ce2223910703eda63
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-18 14:15:20 +0200

    add string methods (part 2)

---
+++ lesson6(27-30)/string-methods-2.js
@@ -0,0 +1,13 @@
+let b = "Learn Code Easily";
+
+console.log(b.indexOf("Code"));
+console.log(b.indexOf("Code", 8));
+console.log(b.indexOf("a"));
+console.log(b.lastIndexOf("a"));
+
+console.log(b.slice(6, 10));
+console.log(b.slice(-6, -3));
+
+console.log(b.repeat(5));
+
+console.log(b.split("", 5));
