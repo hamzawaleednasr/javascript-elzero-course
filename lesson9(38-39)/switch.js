b70ebe552e24de132c5ef93d62ed0df08fc01227
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-18 19:40:52 +0200

    add switch

---
+++ lesson9(38-39)/switch.js
@@ -0,0 +1,14 @@
+let day = 0;
+
+switch (day) {
+    default:
+        console.log("Unknown Day");
+        break;
+    case 0:
+        console.log("Saturday");
+        break;
+    case 1:
+    case 2:
+        console.log("Sunday");
+        break;
+}
