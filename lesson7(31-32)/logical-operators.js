1b450feaae39d0687cfc476d9cae9130fdbbc171
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-18 17:18:31 +0200

    add logical operator

---
+++ lesson7(31-32)/logical-operators.js
@@ -0,0 +1,14 @@
+console.log("10" == 10); // == --> compare value only
+console.log("-10" == -10);
+console.log(10 == 20);
+
+console.log("10" === 10); // === --> compare value and data type
+console.log("-10" === -10);
+console.log(10 === 10);
+
+console.log(10 > 20);
+console.log(10 >= 20);
+console.log(10 < 20);
+console.log(10 <= 20);
+
+console.log(typeof "Hamza" === typeof "Ahmed");
