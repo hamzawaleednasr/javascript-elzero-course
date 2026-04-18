c5144b0f5231a1b7e1167bef85a9a987edb15e59
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-18 13:49:42 +0200

    add string methods

---
+++ lesson6(27-30)/string-methods.js
@@ -0,0 +1,17 @@
+let theName = "  Hamza  ";
+
+console.log(theName.trim());
+
+console.log(theName);
+console.log(theName[2]);
+console.log(theName[10]);
+
+console.log(theName.charAt(2));
+console.log(theName.charAt(10));
+
+console.log(theName.length);
+
+console.log(theName.toUpperCase());
+console.log(theName.toLowerCase());
+
+console.log(theName.trim()[1].toUpperCase());
