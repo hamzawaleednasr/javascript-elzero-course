61182af6ccfe6b9c566115f674da419b04934d6e
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-18 15:04:36 +0200

    add string challenge

---
+++ lesson6(27-30)/string-challenge.js
@@ -0,0 +1,11 @@
+let a = "Elzero Web School";
+
+console.log(a.charAt(2).toUpperCase() + a.slice(3, 6));
+
+console.log(a.charAt(13).toUpperCase().repeat(8));
+
+console.log(a.split(" ", 1));
+
+console.log(`${a.substr(0, 6)} ${a.substr(-6)}`);
+
+console.log(`${a.charAt(0).toLowerCase()}${a.slice(1, -1).toUpperCase()}${a.charAt(a.length - 1).toLowerCase()}`);
