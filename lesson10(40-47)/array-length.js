c202366b15ab49bfe811923c78bedff82b8e3514
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-19 17:40:34 +0200

    add length with arrays

---
+++ lesson10(40-47)/array-length.js
@@ -0,0 +1,14 @@
+let myFriends = ["Omar", "Ayoub", "Mostafa", "Yousef"];
+
+console.log(myFriends.length);
+
+myFriends[myFriends.length] = "Ibraheem";
+myFriends[myFriends.length - 1] = "Asim";
+
+console.log(myFriends);
+console.log(myFriends.length);
+
+myFriends.length = 2;
+
+console.log(myFriends.length);
+console.log(myFriends);
