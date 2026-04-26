ffbbbf98ded37e65a8274ec8be8a15c90a1b6aaa
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-04-26 15:22:08 +0300

    add advanced for loop examples

---
+++ lesson11(48-53)/advanced-for.js
@@ -0,0 +1,9 @@
+let products = ["Keyboard", "Mouse", "Monitor", "Pad", "Lentil", "Wheat"];
+
+let i = 0;
+
+for (;;) {
+    console.log(products[i]);
+    i += 2;
+    if (i >= products.length) break;
+}
