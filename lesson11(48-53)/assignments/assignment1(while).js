17cb9bbfe978582c6cfcc7b97ff0d4e2d0c9f5c0
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-05-02 14:48:49 +0300

    solve while loop assignments

---
+++ lesson11(48-53)/assignments/assignment1(while).js
@@ -0,0 +1,15 @@
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index = 0;
+let counter = 0;
+
+while (counter < friends.length) {
+  if (typeof friends[counter] !== 'number' && friends[counter][index] !== 'A') {
+    console.log(`${counter + true} => ${friends[counter]}`);
+  }
+  counter++;
+}
+
+
+// Output
+"1 => Sayed"
+"2 => Mahmoud"
