10f576c277e6e320747fc9acc2f87b81c5e725ed
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-05-11 10:59:57 +0300

    add advanced functions example

---
+++ lesson12(57-70)/advanced-functions.js
@@ -0,0 +1,10 @@
+function sayHello(name, age) {
+  if (age < 20) {
+    console.log("App is not suitable for you :-(");
+  } else {
+    console.log(`Hello, ${name}!`);
+  }
+}
+
+sayHello("Hamza", 18);
+sayHello("Osama", 45);
