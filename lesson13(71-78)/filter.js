d7fd0ba435e1aa4f30fec76f67b2b16aceb46779
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-06-13 19:21:23 +0300

    add filter lesson

---
+++ lesson13(71-78)/filter.js
@@ -0,0 +1,14 @@
+let friends = ["Ahmed", "Omar", "Hamza", "Ali", "Amr"];
+
+let filteredFriends = friends.filter(friend => friend.startsWith("A"));
+
+console.log(filteredFriends);
+
+// -------------------------
+// -------------------------
+
+let nums = [42, 7, 89, 23, 56, 12, 94, 3, 67, 31];
+
+let evenNums = nums.filter(num => num % 2 === 0);
+
+console.log(evenNums);
