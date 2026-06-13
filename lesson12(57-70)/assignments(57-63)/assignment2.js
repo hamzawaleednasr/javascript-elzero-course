cca41baec8490e5103e203b185cefeb2fc044739
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-06-13 15:44:11 +0300

    solve assignment 2

---
+++ lesson12(57-70)/assignments(57-63)/assignment2.js
@@ -0,0 +1,25 @@
+function calculate(firstNum, secondNum, operation = 'add') {
+  if (secondNum === undefined) {
+    console.log("Second Number Not Found");
+    return;
+  }
+  
+  switch (operation) {
+    case 'add':
+      console.log(firstNum + secondNum);
+      break;
+    case 'subtract':
+      console.log(firstNum - secondNum);
+      break;
+    case 'multiply':
+      console.log(firstNum * secondNum);
+      break;
+  }
+}
+
+// Needed Output
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, 'add'); // 50
+calculate(20, 30, 'subtract'); // -10
+calculate(20, 30, 'multiply'); // 600
