6d45b16d04db7790587682f815190cb41689f82b
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-06-14 12:11:55 +0300

    add advanced examples

--- lesson14(79-85)/objects.js
+++ lesson14(79-85)/objects.js
@@ -1,6 +1,10 @@
+let myVar = "country of";
+
 let user = {
   theName: "Hamza",
   theAge: 18,
+  "country of": "Egypt",
+  100: "salary",
 
   sayHello: _ => `Hello`,
 }
@@ -8,3 +12,6 @@ let user = {
 console.log(user.theName);
 console.log(user.theAge);
 console.log(user.sayHello());
+console.log(user["country of"]);
+console.log(user[myVar]);
+console.log(user["100"]);
