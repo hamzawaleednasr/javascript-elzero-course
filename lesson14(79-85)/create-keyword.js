9bca60a73dbd69b94aa38d16d7d04e4394a8ceaa
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-06-21 11:19:18 +0300

    add create keyword lesson

---
+++ lesson14(79-85)/create-keyword.js
@@ -0,0 +1,23 @@
+let user = {
+  age: 20,
+
+  doubleAge: function () {
+    return this.age * 2;
+  },
+};
+
+console.log(user);
+console.log(user.age);
+console.log(user.doubleAge());
+
+let obj = Object.create({});
+
+console.log(obj);
+
+let copyObj = Object.create(user);
+
+copyObj.age = 10;
+
+console.log(copyObj);
+console.log(copyObj.age);
+console.log(copyObj.doubleAge());
