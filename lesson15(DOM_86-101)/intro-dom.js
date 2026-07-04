9b0ac67c802b080e1b5d578de5e6bcab0ffdb7d4
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-07-04 16:22:55 +0300

    add query selector

--- lesson15(DOM_86-101)/intro-dom.js
+++ lesson15(DOM_86-101)/intro-dom.js
@@ -3,12 +3,16 @@ let myClassElements = document.getElementsByClassName("span");
 let myTagElements = document.getElementsByTagName("p");
 let myTagNSElements = document.getElementsByTagNameNS("*", "h1");
 let myRootNode = document.getRootNode();
+let myQueryElement = document.querySelector("html body .special");
+let myQueryAllElements = document.querySelectorAll("span");
 
 console.log(myIdElement);
 console.log(myClassElements[0]);
 console.log(myTagElements[1]);
 console.log(myTagNSElements[0]);
 console.log(myRootNode);
+console.log(myQueryElement);
+console.log(myQueryAllElements);
 myIdElement.innerHTML = "NO Please . . . . ."
 
 console.log(document.title);
