3e53555f6119629d701dfdc6e4025781a2c0e00c
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-07-04 19:32:12 +0300

    add access childs lesson

---
+++ lesson15(DOM_86-101)/children/main.js
@@ -0,0 +1,10 @@
+let myElement = document.querySelector("div");
+
+console.log(myElement.children);
+console.log(myElement.childNodes);
+console.log(myElement.childNodes[0]);
+console.log(myElement.firstChild);
+console.log(myElement.lastChild);
+console.log(myElement.firstElementChild);
+console.log(myElement.lastElementChild);
+console.log(myElement.hasChildNodes());
