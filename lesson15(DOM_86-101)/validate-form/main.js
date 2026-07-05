d620c8f2d45d6dbaa0087a2b3d3e8d0634c6316d
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-07-05 12:23:00 +0300

    add validate forms lesson

---
+++ lesson15(DOM_86-101)/validate-form/main.js
@@ -0,0 +1,16 @@
+let userInput = document.querySelector(`[name="username"]`);
+let ageInput = document.querySelector(`[name="age"]`);
+
+document.forms[0].onsubmit = function (e) {
+  let userValid = false;
+  let ageValid = false;
+
+  if (userInput.value !== "" && userInput.value.length <= 10)
+    userValid = true;
+
+  if (ageInput.value !== "")
+    ageValid = true;
+
+  if (userValid === false || ageValid === false)
+    e.preventDefault();
+}
