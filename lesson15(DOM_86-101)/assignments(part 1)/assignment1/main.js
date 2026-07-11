a1a4a36dfa741f992b587e0d07d7e305d307ad01
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-07-11 15:21:00 +0300

    solve assignment2 part 1

--- lesson15(DOM_86-101)/assignments(part 1)/assignment1/main.js
+++ lesson15(DOM_86-101)/assignments(part 1)/assignment1/main.js
@@ -1,32 +1,32 @@
-let e = document.getElementById("elzero");
+let e = document.getElementById("elzero"); // 1
 console.log(e);
-e = document.getElementsByClassName("element")[0];
+e = document.getElementsByClassName("element")[0]; // 2
 console.log(e);
-e = document.getElementsByName("js")[0];
+e = document.getElementsByName("js")[0]; // 3
 console.log(e);
-e = document.getElementsByTagName("div")[0];
+e = document.getElementsByTagName("div")[0]; // 4
 console.log(e);
-e = document.querySelector("div");
+e = document.querySelector("div"); // 5
 console.log(e);
-e = document.querySelector("#elzero");
+e = document.querySelector("#elzero"); // 6
 console.log(e);
-e = document.querySelector(".element");
+e = document.querySelector(".element"); // 7
 console.log(e);
-e = document.querySelector(`[name="js"]`);
+e = document.querySelector(`[name="js"]`); // 8
 console.log(e);
-e = document.querySelectorAll("div")[0];
+e = document.querySelectorAll("div")[0]; // 9
 console.log(e);
-e = document.querySelectorAll("#elzero")[0];
+e = document.querySelectorAll("#elzero")[0]; // 10
 console.log(e);
-e = document.querySelectorAll(".element")[0];
+e = document.querySelectorAll(".element")[0]; // 11
 console.log(e);
-e = document.querySelectorAll(`[name="js"]`)[0];
+e = document.querySelectorAll(`[name="js"]`)[0]; // 12
 console.log(e);
-e = document.body.firstChild;
+e = document.body.firstChild; // 13
 console.log(e);
-e = document.body.firstElementChild;
+e = document.body.firstElementChild; // 14
 console.log(e);
-e = document.body.children[0];
+e = document.body.children[0]; // 15
 console.log(e);
-e = document.body.childNodes[0];
+e = document.body.childNodes[0]; // 16
 console.log(e);
---
+++ lesson15(DOM_86-101)/assignments(part 1)/assignment2/main.js
@@ -0,0 +1,6 @@
+let images = document.images;
+
+for (let i = 0; i < images.length; i++) {
+  images[i].src = "https://elzero.org/wp-content/themes/elzero/imgs/logo.png";
+  images[i].alt = "Elzero Logo";
+}
