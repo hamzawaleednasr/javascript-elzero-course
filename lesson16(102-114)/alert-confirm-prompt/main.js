ca95f28781aa6943ae3b7a8f412fd17b50a88f84
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-07-11 23:29:44 +0300

    add alert & confirm & prompt lesson

---
+++ lesson16(102-114)/alert-confirm-prompt/main.js
@@ -0,0 +1,18 @@
+window.alert("Text");
+this.alert("Text");
+alert("Text");
+
+console.log("OK!");
+
+let confirmMsg = window.confirm("Are You Sure?");
+
+if (confirmMsg) {
+  console.log("Item deleted");
+} else {
+  console.log("Item not deleted");
+}
+
+let promptMsg = window.prompt("Good Day?", "Write day in 3 chars format.");
+
+console.log(promptMsg);
+  
