3b9eeba8a13da82b72cfaf04e3861011ba2701ee
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-07-11 19:04:09 +0300

    add deal with elements lesson

---
+++ lesson15(DOM_86-101)/deal-with-elements/main.js
@@ -0,0 +1,10 @@
+let element = document.getElementById("my-div");
+let newParagraph = document.createElement("p");
+
+element.after(newParagraph);
+element.before("Hello from JS");
+
+newParagraph.innerHTML = "Paragraph From JS";
+
+element.prepend(newParagraph);
+element.append("Bye, bye! (FROM JS)");
