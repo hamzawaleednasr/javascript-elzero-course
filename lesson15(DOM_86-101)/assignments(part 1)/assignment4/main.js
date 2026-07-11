1f12afb6b8345d75affd1aae1c83798f02f5202c
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-07-11 16:10:39 +0300

    solve assigment4 part 1

---
+++ lesson15(DOM_86-101)/assignments(part 1)/assignment4/main.js
@@ -0,0 +1,8 @@
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+
+one.innerHTML = "One";
+two.innerHTML = "Two 2";
+
+one.setAttribute("title", "one");
+two.setAttribute("title", "two");
