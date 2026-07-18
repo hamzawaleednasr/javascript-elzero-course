39fdcdc5870afecf54b5688e5d2689c11e3d1ffb
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-07-18 11:50:26 +0300

    add location object lesson

---
+++ lesson16(102-114)/location-object/main.js
@@ -0,0 +1,13 @@
+console.log(location.href);
+// location.href = "https://google.com";
+// location.href = "http://127.0.0.1:5500/lesson16(102-114)/location-object/#two";
+// location.href = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return#examples";
+
+console.log(location.host);
+console.log(location.hostname);
+
+console.log(location.protocol);
+
+// location.reload();
+// location.replace("https://google.com");
+// location.assign("https://google.com");
