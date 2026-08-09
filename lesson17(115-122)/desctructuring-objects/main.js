767a23b4e6a5469950cf4b3a518937925a067865
Hamza Waleed <hamzamuhammad.9825@azhar.edu.eg>
2026-08-09 15:56:21 +0300

    add destructuring objects lesson

---
+++ lesson17(115-122)/desctructuring-objects/main.js
@@ -0,0 +1,12 @@
+let user = {
+  theName: "Hamza",
+  theAge: 25,
+  theTitle: "Developer",
+  theCountry: "Egypt",
+};
+
+let { theName, theAge, theCountry } = user;
+
+console.log(theName);
+console.log(theAge);
+console.log(theCountry);
